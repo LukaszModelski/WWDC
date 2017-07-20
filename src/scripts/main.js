005e6e6f3aba16cde9b5b7580b9fee6f2523b6ef
Czapa <lukaszmodelski90@gmail.com>
2017-07-21 00:38:11 +0200

    Working on RWD

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,7 +2,7 @@
 $(function() {
     $('.chart1').easyPieChart({
     	trackColor:	'rgb(229,229,227)',
-        size: '230',
+        size: '200',
         lineWidth: '5',
         barColor: 'rgb(243,71,57)',
         scaleColor: false
@@ -12,7 +12,7 @@ $(function() {
 $(function() {
     $('.chart2').easyPieChart({
     	trackColor:	'rgb(229,229,227)',
-        size: '230',
+        size: '200',
         lineWidth: '5',
         barColor: 'rgb(0,153,137)',
         scaleColor: false
@@ -22,7 +22,7 @@ $(function() {
 $(function() {
     $('.chart3').easyPieChart({
     	trackColor:	'rgb(229,229,227)',
-        size: '230',
+        size: '200',
         lineWidth: '5',
         barColor: 'rgb(21,43,60)',
         scaleColor: false
